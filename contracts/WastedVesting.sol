6cdc279042a5283810bc5162e2370402e0499442
Nightmare <hnbtranspkt@gmail.com>
2022-03-12 17:56:20 +0700

    [Update] update vesting

--- contracts/WastedVesting.sol
+++ contracts/WastedVesting.sol
@@ -9,11 +9,20 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./utils/TokenWithdrawable.sol";
 
-contract WastedVesting is EIP712, AccessControl, ReentrancyGuard {
+contract WastedVesting is
+    EIP712,
+    AccessControl,
+    ReentrancyGuard,
+    TokenWithdrawable
+{
     using SafeMath for uint256;
 
+    event Claimed(address investorAddress, uint256 totalClaim, uint256 claimId);
+
     struct Investor {
+        address investorAddress;
         uint256 totalClaim;
         uint256 claimId;
     }
@@ -21,26 +30,16 @@ contract WastedVesting is EIP712, AccessControl, ReentrancyGuard {
     IERC20 public acceptedToken;
     mapping(address => Investor) public _investors;
     mapping(uint256 => bool) public isClaimed;
-    address private serverAddress;
     bytes32 public constant SERVER_ROLE = keccak256("SERVER_ROLE");
     bytes32 public constant CONTROLLER_ROLE = keccak256("CONTROLLER_ROLE");
     string private constant SIGNING_DOMAIN = "LazyClaiming-WastedLands";
     string private constant SIGNATURE_VERSION = "1";
 
-    constructor(address _serverAddress, IERC20 _acceptedToken)
+    constructor(IERC20 _acceptedToken)
         EIP712(SIGNING_DOMAIN, SIGNATURE_VERSION)
     {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        serverAddress = _serverAddress;
         acceptedToken = _acceptedToken;
-    }
-
-    function setServerAddress(address _serverAddress)
-        external
-        onlyRole(CONTROLLER_ROLE)
-    {
-        require(_serverAddress != address(0));
-        serverAddress = _serverAddress;
     }
 
     function claim(Investor calldata investor, bytes memory signature)
@@ -50,6 +49,7 @@ contract WastedVesting is EIP712, AccessControl, ReentrancyGuard {
         uint256 totalClaim = investor.totalClaim;
         address signer = _verify(investor, signature);
 
+        require(msg.sender == investor.investorAddress, "WAV: invalid sender");
         require(!isClaimed[investor.claimId], "WAV: already claimed");
         require(
             hasRole(SERVER_ROLE, signer),
@@ -62,6 +62,8 @@ contract WastedVesting is EIP712, AccessControl, ReentrancyGuard {
         isClaimed[investor.claimId] = true;
         acceptedToken.transfer(msg.sender, totalClaim);
         _investors[msg.sender] = investor;
+
+        emit Claimed(msg.sender, totalClaim, investor.claimId);
     }
 
     function _hash(Investor calldata investor) internal view returns (bytes32) {
@@ -70,10 +72,11 @@ contract WastedVesting is EIP712, AccessControl, ReentrancyGuard {
                 keccak256(
                     abi.encode(
                         keccak256(
-                            "Investor(uint256 totalClaim,uint256 claimId)"
+                            "Investor(address investorAddress,uint256 totalClaim,uint256 claimId)"
                         ),
-                        keccak256(abi.encodePacked(investor.totalClaim)),
-                        keccak256(abi.encodePacked(investor.claimId))
+                        investor.investorAddress,
+                        investor.totalClaim,
+                        investor.claimId
                     )
                 )
             );
@@ -94,11 +97,5 @@ contract WastedVesting is EIP712, AccessControl, ReentrancyGuard {
     {
         bytes32 digest = _hash(investor);
         return ECDSA.recover(digest, signature);
-    }
-
-    function withdraw(uint256 amount) external onlyRole(CONTROLLER_ROLE) {
-        uint256 balance = acceptedToken.balanceOf(address(this));
-        require(balance >= amount, "WAV: sufficient balance");
-        acceptedToken.transfer(msg.sender, amount);
     }
 }
--- migrations/1_deploy_vesting.js
+++ migrations/1_deploy_vesting.js
@@ -2,5 +2,5 @@ var WastedVesting = artifacts.require("WastedVesting");
 
 module.exports = function(deployer) {
   // deployment steps
-  deployer.deploy(WastedVesting,"0x0329958a15ad1d9ba7836425c998c69a92067cc0");
+  deployer.deploy(WastedVesting, "0xA4441b7f6FD28814e29116C7bdeF0574e8288B8E");
 };
